4bdd6fe6cba50a56ea4a0b858666e0da7815e76d
liqi328 <liqi328@163.com>
2013-07-10 16:18:48 +0800

    重构IDConvertor

--- src/id/EntrezIDConvertor.java
+++ src/id/EntrezIDConvertor.java
@@ -1,12 +1,9 @@
 package id;
 
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 import reader.DiseaseGeneSeedReader;
-
 import util.WriterUtil;
 
 /**
@@ -15,74 +12,11 @@ import util.WriterUtil;
  *
  */
 class EntrezIDConvertor extends IDConvertor{
-
-	@Override
-	public Set<String> converte2hprdId(Set<String> entrezIdSet) {
-		Map<String, HprdIdMapping> entrezIdIndexedIdMappingMap = getHprdIdMappingMap();
-		
-		Set<String> hprdIdSet = new HashSet<String>();
-		Iterator<String> itr = entrezIdSet.iterator();
-		while(itr.hasNext()){
-			String entrezId = itr.next();
-			HprdIdMapping idMapping = entrezIdIndexedIdMappingMap.get(entrezId);
-			addOneHprdId(hprdIdSet, entrezId, idMapping);
-		}
-		
-		return hprdIdSet;
-	}
-
-	private void addOneHprdId(Set<String> hprdIdSet, String entrezId,
-			HprdIdMapping idMapping) {
-		if(idMapping != null){
-			hprdIdSet.add(String.valueOf(Integer.parseInt(idMapping.getHrpdId())));
-		}else{
-			System.out.println("entrez_id: "+ entrezId + " has not find corresponding hprd_id.");
-		}
-	}
-	
-	@Override
-	public Set<String> converte2omimId(Set<String> entrezIdSet) {
-		Map<String, HprdIdMapping> entrezIdIndexedIdMappingMap = getHprdIdMappingMap();
-		
-		Set<String> hprdIdSet = new HashSet<String>();
-		Iterator<String> itr = entrezIdSet.iterator();
-		while(itr.hasNext()){
-			String entrezId = itr.next();
-			HprdIdMapping idMapping = entrezIdIndexedIdMappingMap.get(entrezId);
-			if(idMapping != null){
-				hprdIdSet.add(String.valueOf(Integer.parseInt(idMapping.getOmimId())));
-			}else{
-				System.out.println("entrez_id: "+ entrezId + " has not find corresponding omim_id.");
-			}
-		}
-		
-		return hprdIdSet;
-	}
-	
-
-	@Override
-	public Set<String> converte2entrezId(Set<String> entrezIdSet) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
 	@Override
 	protected Map<String, HprdIdMapping> getHprdIdMappingMap() {
 		return HprdIdMappingUtil.getEntrezIdIndexedIdMapping();
 	}
 
-
-	public Set<HprdIdMapping> getHprdIdMappingSet2(Set<String> entrezIdSet) {
-		Map<String, HprdIdMapping> entrezIdIndexedIdMappingMap = HprdIdMappingUtil.getEntrezIdIndexedIdMapping();
-		
-		Set<HprdIdMapping> result = new HashSet<HprdIdMapping>();
-		Iterator<String> itr = entrezIdSet.iterator();
-		while(itr.hasNext()){
-			result.add(entrezIdIndexedIdMappingMap.get(itr.next()));
-		}
-		return result;
-	}
-	
 	public static void main(String[] args){
 		String filename = "E:/2013疾病研究/实验数据/prioritizing_candidate_gene/orphanet/Achromatopsia_entrez_id.txt";
 		Set<String> entrezIdset = DiseaseGeneSeedReader.read(filename);
--- src/id/IDConvertor.java
+++ src/id/IDConvertor.java
@@ -7,12 +7,94 @@ import java.util.Set;
 
 
 public abstract class IDConvertor{
-	public abstract Set<String> converte2hprdId(Set<String> idSet);
-	public abstract Set<String> converte2entrezId(Set<String> idSet);
-	public abstract Set<String> converte2omimId(Set<String> idSet);
+	/**
+	 * 将entrez_id集合或者omim_id集合转换成hprd_id集合
+	 * @param idSet		entrez_id集合或者omim_id集合
+	 * @return			hprd_id集合
+	 */
+	public Set<String> converte2hprdId(Set<String> idSet){
+		Map<String, HprdIdMapping> idIndexedIdMappingMap = getHprdIdMappingMap();
+		
+		Set<String> hprdIdSet = new HashSet<String>();
+		Iterator<String> itr = idSet.iterator();
+		while(itr.hasNext()){
+			String id = itr.next();
+			HprdIdMapping idMapping = idIndexedIdMappingMap.get(id);
+			
+			if(idMapping != null){
+				hprdIdSet.add(String.valueOf(Integer.parseInt(idMapping.getHrpdId())));
+			}else{
+				System.out.println("id: " + id + " has not find corresponding hprd_id.");
+			}
+		}
+		
+		return hprdIdSet;
+	}
 	
+	/**
+	 * 将hprd_id集合或者omim_id集合转换成entrez_id集合
+	 * @param idSet	hprd_id集合或者omim_id集合
+	 * @return		entrez_id集合
+	 */
+	public Set<String> converte2entrezId(Set<String> idSet){
+		Map<String, HprdIdMapping> idIndexedIdMappingMap = getHprdIdMappingMap();
+		
+		Set<String> entrezIdSet = new HashSet<String>();
+		Iterator<String> itr = idSet.iterator();
+		while(itr.hasNext()){
+			String id = itr.next();
+			HprdIdMapping idMapping = idIndexedIdMappingMap.get(id);
+			if(idMapping != null){
+				entrezIdSet.add(String.valueOf(Integer.parseInt(idMapping.getEntrezGeneId())));
+			}else{
+				System.out.println("id: " + id + " has not find corresponding entrez_id.");
+			}
+		}
+		return entrezIdSet;
+	}
+	
+	/**
+	 * 将hprd_id集合或者entrez_id集合转换成omim_id集合
+	 * @param idSet		hprd_id集合或者entrez_id集合
+	 * @return			omim_id集合
+	 */
+	public Set<String> converte2omimId(Set<String> idSet){
+		Map<String, HprdIdMapping> entrezIdIndexedIdMappingMap = getHprdIdMappingMap();
+		
+		Set<String> omimIdSet = new HashSet<String>();
+		Iterator<String> itr = idSet.iterator();
+		while(itr.hasNext()){
+			String id = itr.next();
+			HprdIdMapping idMapping = entrezIdIndexedIdMappingMap.get(id);
+			if(idMapping != null){
+				omimIdSet.add(String.valueOf(Integer.parseInt(idMapping.getOmimId())));
+			}else{
+				System.out.println("id: "+ id + " has not find corresponding omim_id.");
+			}
+		}
+		
+		return omimIdSet;
+	}
+	
+	/**
+	 * 利用HprdIdMappingUtil获取
+	 * 	entrezIdIndexIdMappingMap,
+	 * 	hprdIdIndexedIdMappingMap,
+	 *  omimIdIndexedIdMappingMap 三者之一, <br />
+	 * 不同的IDConvertor需要不同的idIndexIdMappingMap <br />
+	 * @return
+	 */
+	protected abstract Map<String, HprdIdMapping> getHprdIdMappingMap();
+	
+	
+	/**
+	 * 根据给定的id集合，得到其相应的HprdIdMapping
+	 * @param idSet		id为entrez_id, omim_id, hprd_id中三者之一
+	 * @return
+	 */
 	public final Set<HprdIdMapping> getHprdIdMappingSet(Set<String> idSet){
 		Map<String, HprdIdMapping> idIndexedIdMappingMap = getHprdIdMappingMap();
+		
 		Set<HprdIdMapping> result = new HashSet<HprdIdMapping>();
 		Iterator<String> itr = idSet.iterator();
 		while(itr.hasNext()){
@@ -26,6 +108,4 @@ public abstract class IDConvertor{
 		}
 		return result;
 	}
-	
-	protected abstract Map<String, HprdIdMapping> getHprdIdMappingMap();
 }
