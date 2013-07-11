955981e164fcb355734a61f9827996cd240eeca8
liqi328 <liqi328@163.com>
2013-07-11 14:36:07 +0800

    批量执行实验

---
+++ src/experiment/BatchMain.java
@@ -0,0 +1,42 @@
+package experiment;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BatchMain {
+	public static void main(String[] args){
+		if(args.length != 1){
+			System.out.println("Argument Error.");
+			System.out.println("Using method: java -Xmx2048m -jar Prioritizing.jar ./input/config.txt");
+			System.exit(-1);
+		}
+		
+		List<String> configFilenames = readConfigFilenames(args[0]);
+		
+		int i = 1;
+		for(String config : configFilenames){
+			System.out.println("" + (i++) + "--------------------"+ config +"-------------------");
+			Main.main(new String[]{config});
+		}
+		
+	}
+	
+	private static List<String> readConfigFilenames(String filename){
+		List<String> configFilenames = new ArrayList<String>();
+		String line = null;
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(new File(filename)));
+			while((line = in.readLine()) != null){
+				configFilenames.add(line.trim());
+			}
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return configFilenames;
+	}
+}
